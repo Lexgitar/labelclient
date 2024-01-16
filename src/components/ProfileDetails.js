5b7cf9180cf857134ca2b3752229286c4deeef92
Lexgitar <berdea.alex@yahoo.com>
2024-01-16 20:32:39 +0000

    redux state update

--- src/components/ProfileDetails.js
+++ src/components/ProfileDetails.js
@@ -8,61 +8,63 @@ import { attachUser, selectUserInfo, selectLoggedIn } from '../slices/userSlice'
 const ProfileDetails = () => {
     const dispatch = useDispatch()
     const navigate = useNavigate()
-//if logged see button
+    //  
     const loggedIn = useSelector(selectLoggedIn)
-//if not submitted see button
-//if role not same see button
-    let userRole = useSelector(selectUser).role
-    console.log(userRole)
-   
-
+    const userRole = useSelector(selectUser).role 
+    const userRoleInfo = useSelector(selectUserInfo)
+    //console.log(userRole)
+    //
     let bands = useSelector(selectRoles).bands
     let labels = useSelector(selectRoles).labels
-    
-    const userInfo = useSelector(selectUserInfo) 
-    //console.log('userinfo ', userInfo)
-
+    //
     const { id } = useParams()
     let usersPool = useLocation().pathname.includes('labels') ? labels : bands
-    let urlRole = useLocation().pathname.includes('labels')? 'labels' : 'bands'
-
-    let byRolechecking = (`${userRole}s` == urlRole)? false : true
+    let roleFromUrl = useLocation().pathname.includes('labels') ? 'labels' : 'bands'
+    let fanRoleCheck = userRole === 'fan'? false : true
+    let byRolechecking = (`${userRole}s` == roleFromUrl) ? false : true
     //console.log(byRolechecking)
 
-    const userKind =  usersPool.filter(function (user) {
+    const userProfile = usersPool.filter(function (user) {
         return user._id == id
     })[0]
-    //console.log('userkind ', userKind)
+    //console.log('userkind ', userProfile)
 
-    let notInArrayCheck = userKind.attachedId.includes(userInfo._id)? false : true
+    let notInArrayCheck = userProfile.attachedId.includes(userRoleInfo._id) ? false : true
     //console.log('araycheck ', notInArrayCheck)
-    
+
     const handleAttach = () => {
-        if(userInfo && userKind){
-            const attachId = userInfo._id
-            const hostId = userKind._id
-        console.log('handle attach', attachId, hostId)
-        dispatch(attachUser({urlRole, hostId, attachId }))
-        navigate('/')
+        if (userRoleInfo && userProfile) {
+            
+            const attachId = userRoleInfo._id
+            const hostId = userProfile._id
+            console.log('handle attach', attachId, hostId)
+            dispatch(attachUser({ roleFromUrl, hostId, attachId }))
+            navigate('/')
         }
-        
+
 
     }
 
-    if (userKind) {
+    if (userProfile) {
         return (
             <div>
-                <p>_id:{userKind._id}</p>
+                <p>_id:{userProfile._id}</p>
 
-                <p>name: {userKind.name}</p>
-                <p>location:{userKind.location}</p>
-                <p>atId{userKind.attachedId}</p>
-               {loggedIn && byRolechecking && notInArrayCheck && <button 
-               onClick={handleAttach}
-               >Submit</button>
-               }
-                <p>{userInfo._id}</p>
+                <p>name: {userProfile.name}</p>
+                <p>location:{userProfile.location}</p>
+                <p>atId{userProfile.attachedId}</p>
+                {loggedIn &&
+                 byRolechecking &&
+                  notInArrayCheck &&
+                   fanRoleCheck &&
+                   <button
+                    onClick={handleAttach}
+                   >
+                    Submit
+                   </button>
+                }
                 
+
             </div>
 
         )
--- src/slices/userSlice.js
+++ src/slices/userSlice.js
@@ -2,12 +2,12 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 const initialState = {
-    user: [],
+    user: '',
     loggedIn: false,
     status: '',
     error: '',
     userInfo: '',
-    edit:false,
+    edit: false,
     roles: {
         bands: [],
         labels: [],
@@ -17,13 +17,13 @@ const initialState = {
 
 }
 
-export const attachUser= createAsyncThunk('userDetails/attachUser',
-    async ({urlRole, hostId, attachId}) => {
-        let link = `/api/${urlRole}/${hostId}?attach=${attachId}`
+export const attachUser = createAsyncThunk('userDetails/attachUser',
+    async ({ roleFromUrl, hostId, attachId }) => {
+        let link = `/api/${roleFromUrl}/${hostId}?attach=${attachId}`
         const response = await axios.put(link)
         const array = await response.data
-        console.log('aU', hostId, attachId, link,  array)
-        return array
+        console.log('aU', hostId, attachId, link, array)
+        return { roleFromUrl, hostId, array }
 
     }
 )
@@ -33,13 +33,13 @@ export const createDetails = createAsyncThunk('userDetails/createDetails',
         const response = await axios.post(`api/${role}s`, body)
         const user = await response.data
         console.log('uS', role, response.data)
-        return {user, role}
+        return { user, role }
 
     }
 )
 
 export const editDetails = createAsyncThunk('userDetails/editDetails',
-    async ({id, body, role }) => {
+    async ({ id, body, role }) => {
         const response = await axios.put(`api/${role}s/${id}`, body)
         const user = response.data
         console.log('uSedit', role, response.data)
@@ -66,7 +66,7 @@ export const userSlice = createSlice({
     initialState,
     reducers: {
         addUser: (state, action) => {
-            state.user.push(action.payload)
+            state.user = action.payload
 
         },
         toggleLog: (state, action) => {
@@ -74,16 +74,16 @@ export const userSlice = createSlice({
             //if state empty logged in = false
         },
         userDelete: (state) => {
-            state.user.pop()
+            state.user = ''
         },
 
         addUserInfo: (state, action) => {
             state.userInfo = action.payload
         },
-        toggleEdit:(state,action)=>{
+        toggleEdit: (state, action) => {
             state.edit = action.payload
         },
-       
+
     },
     extraReducers(builder) {
         builder
@@ -98,17 +98,20 @@ export const userSlice = createSlice({
                 state.status = 'succeeded'
                 state.error = ''
                 state.userInfo = action.payload.user
-                const arrayByRole = (action.payload.role === 'band' ? state.roles.bands : (action.payload.role   === 'label' ? state.roles.labels : state.roles.fans))
-               arrayByRole.push(action.payload.user)
-                
+                const arrayByRole = (action.payload.role === 'band' ? state.roles.bands : (action.payload.role === 'label' ? state.roles.labels : state.roles.fans))
+                arrayByRole.push(action.payload.user)
+
 
             })
             .addCase(editDetails.fulfilled, (state, action) => {
                 state.status = 'succeeded'
                 state.error = ''
                 state.userInfo = action.payload
-                
-
+                const arrayByRole = (state.user.role === 'band' ? state.roles.bands : (state.user.role === 'label' ? state.roles.labels : state.roles.fans))
+                //edge case when user is null
+                //    console.log('addcase',arrayByRole.findIndex((item)=>item._id === action.payload._id)) 
+                let editedIndex = arrayByRole.findIndex((item) => item._id === action.payload._id)
+                arrayByRole.splice(editedIndex, 1, action.payload)
             })
             .addCase(fetchRoles.fulfilled, (state, action) => {
 
@@ -116,16 +119,22 @@ export const userSlice = createSlice({
                 state.roles.labels = action.payload[1]
                 state.roles.fans = action.payload[2]
             })
+            .addCase(attachUser.fulfilled, (state, action) => {
+                const arrayByRole = state.user.role === 'band' ? state.roles.labels : state.roles.bands
+                console.log('attUsr', arrayByRole)
+                let arrayIndex = arrayByRole.findIndex((item)=> item._id === action.payload.hostId)
+                arrayByRole[arrayIndex].attachedId = action.payload.array
+            })
     }
 })
 
 export default userSlice.reducer
 
-export const { addUser, toggleLog, userDelete, initedToggle, addUserInfo,toggleEdit, addRole } = userSlice.actions
-export const selectUser = (state) => state.user.user[0]
+export const { addUser, toggleLog, userDelete, initedToggle, addUserInfo, toggleEdit, addRole } = userSlice.actions
+export const selectUser = (state) => state.user.user
 export const selectLoggedIn = (state) => state.user.loggedIn
 export const selectStatus = (state) => state.user.status
 export const selectError = (state) => state.user.error
 export const selectUserInfo = (state) => state.user.userInfo
-export const selectUserEdit = (state)=> state.user.edit
+export const selectUserEdit = (state) => state.user.edit
 export const selectRoles = (state) => state.user.roles
