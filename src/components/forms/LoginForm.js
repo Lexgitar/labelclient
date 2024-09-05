58127729cee415d85b0149b3083abd0b366a16cf
Lexgitar <berdea.alex@yahoo.com>
2024-09-05 23:49:50 +0100

    opts

--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -31,7 +31,9 @@ const LoginForm = () => {
         e.preventDefault()
         let body = { email, password }
         try {
-            const response = await axios.post(`${baseUrl}/api/login`, body)
+            const response = await axios.post(`${baseUrl}/api/login`, body, {
+                withCredentials: true
+            })
 
             if (response.data && response.data.status !== 400) {
 
