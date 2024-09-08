3d5e056f59cfc509ee64df0f2142e3d71dbb9976
Lexgitar <berdea.alex@yahoo.com>
2024-09-08 13:16:43 +0100

    nmo xrfs

--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -45,7 +45,7 @@ const LoginForm = () => {
 
                 {
                     withCredentials: true,
-                    xsrfCookieName: 'jwt',
+                   
 
                 }
             )
