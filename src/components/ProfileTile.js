766d382984a43612bab76351384e57a181839799
Lexgitar <berdea.alex@yahoo.com>
2024-09-09 23:00:33 +0100

    tidy

--- src/components/ProfileTile.js
+++ src/components/ProfileTile.js
@@ -5,11 +5,6 @@ import { selectRoles } from '../slices/userSlice'
 import { useSelector } from 'react-redux'
 
 
-
-
-
-
-
 const ProfileTile = ({ profile }) => {
 
     let bands = useSelector(selectRoles).bands
@@ -36,44 +31,24 @@ const ProfileTile = ({ profile }) => {
         })
 
         return <Link to={path} key={id}>id:{name}</Link>
-
     }
 
 
-    //console.log('PTile', profile)
-    // console.log('patnew', pathNew)
     return (
 
         <div id="ptile" >
             <Link key={profile.id} to={`${profile._id}`}>
-                {/* <p>id: {profile._id}</p> */}
                 <p>Name: {profile.name}</p>
                 <p>Location: {profile.location}</p>
                 {profile.genre && <p>Genre: {profile.genre}</p>}
-                {/* <p>About: {profile.about}</p> */}
                 <p>Link: {profile.links}</p>
-                {/* <p>role: {profile.role}</p> */}
                 <p>Collabs : {(!!profile.attachedId.length && profile.attachedId.length) || ' 0'}</p>
             </Link>
-            {/* {!!profile.attachedId.length && profile.attachedId.map((id) => (
-
-
-                // <Link to={roleFinder(id)} key={id}>id:{id}</Link>
-                roleFinder(id)
-
-
-            ))} */}
-
-
 
         </div>
     )
 
-
-
 }
-
-
 
 
 export default ProfileTile
